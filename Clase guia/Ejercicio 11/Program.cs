5ac19bb6807651a623d797c6ac1ae896cbf621a0
tinchogodoy <36281666+tinchogodoy@users.noreply.github.com>
2018-08-27 22:28:50 -0300

    clase2

---
+++ Clase guia/Ejercicio 11/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Ejercicio_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string buferValor = "";
+            int valor = 0;
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            float promedio = 0;
+            bool esNumero = false;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("\ningrese el numero: ");
+                buferValor = Console.ReadLine();
+                if (esNumero = int.TryParse(buferValor, out valor))
+                {
+                   if(Ejercicio11.Validacion.Validar(valor, min, max)
+                   {
+                        acumulador = acumulador + numero;
+                        if (flag == 0)
+                        {
+                            max = valor;
+                            min = valor;
+                            flag = 1;
+                        }
+                        if ( valor < min)
+                        {
+                            min = valor;
+                        }
+                        else if (valor>max)
+                        {
+                            max = valor;
+                        }
+                    }
+                    else
+                    {
+
+                        Console.WriteLine("ingrese solo numeros...");
+                        i--;
+                    }
+                }
+                    
+            }
+                promedio = acumulador / i;
+                Console.WriteLine("Máximo: " + max);
+                Console.WriteLine("Mínimo: " + min;
+                Console.WriteLine("Promedio: " + promedio);
+                Console.ReadKey();
+                Console.ReadKey();
+        }
+    }
+}
