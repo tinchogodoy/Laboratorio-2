6721d5de72efe5b12df72110c4f37169e0467741
alumno <alumno>
2018-08-29 22:29:12 -0300

    Primer commit como la gente

---
+++ Clase guia/Ejercicio11bis/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio11bis
+{
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                string buferValor = "";
+                int valor = 0;
+                int max = int.MinValue;
+                int min = int.MaxValue;
+                float promedio = 0;
+                bool esNumero = false;
+                int acumulador = 0;
+                int flag = 0;
+                int i;
+                for ( i = 0; i < 10; i++)
+                {
+                    Console.WriteLine("\ningrese el numero: ");
+                    buferValor = Console.ReadLine();
+                    if (esNumero = int.TryParse(buferValor, out valor))
+                    {
+                        if (Ejercicio11.Validacion.Validar(valor, min, max))
+                        {
+                            acumulador = acumulador + valor;
+                            if (flag == 0)
+                            {
+                                max = valor;
+                                min = valor;
+                                flag = 1;
+                            }
+                            if (valor < min)
+                            {
+                                min = valor;
+                            }
+                            else if (valor > max)
+                            {
+                                max = valor;
+                            }
+                        }
+                        else
+                        {
+
+                            Console.WriteLine("ingrese mayor igual a -100 y menor igual a 100...");
+                            i--;
+                        }
+                    }
+
+                }
+                promedio = acumulador / i;
+                Console.WriteLine("Máximo: " + max);
+                Console.WriteLine("Mínimo: " + min);
+                Console.WriteLine("Promedio: " + promedio);
+                Console.ReadKey();
+                
+            }
+        }
+    
+}
---
+++ Clase guia/Ejercico 14/CalculoDeArea.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercico_14
+{
+    public class CalculoDeArea
+    {
+        static double CalcularCuadrado(double lado)
+        {
+            double areaCuadrado = 0;
+
+            areaCuadrado = lado * lado;
+
+            return areaCuadrado;
+
+        }
+
+        static double CalcularTriangulo(double alturaA , double baseB)
+        {
+            double areaTriangulo = 0;
+
+            areaTriangulo = (baseB * alturaA) / 2;
+
+            return areaTriangulo;
+
+        }
+
+
+        static double CalcularCirculo(double radio)
+        {
+            double areaCirculo = 0;
+
+            areaCirculo = Math.PI * Math.Pow(radio, 2);
+
+            return areaCirculo;
+
+        }
+    }
+}
