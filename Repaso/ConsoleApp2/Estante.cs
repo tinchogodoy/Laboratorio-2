61ac9c3077dec2ecc9b59b76187b9d03a677281f
alumno <alumno>
2018-09-05 22:31:04 -0300

    ejercicio clase 5

---
+++ Repaso/ConsoleApp2/Estante.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Estante
+    {
+
+        private Producto[] producto;
+        private int ubicacionEstante;
+
+        private Estante(int capacidad)
+        {
+            this.producto = new Producto[capacidad];
+        }
+       
+        public Estante(int capacidad, int ubicacion) : this(capacidad)
+        {
+            this.ubicacionEstante = ubicacion;
+        }
+        
+        public Producto[] GetProductos()
+        {
+            return this.producto;
+        }
+
+        public string MostrarEstante(Estante estanteA)
+        {
+            string bufferEstante;
+            bufferEstante = string.Format(" Ubicacion: {0} ", estanteA.ubicacionEstante);
+
+            for (int i=0; i<estanteA.producto.Length; i++)
+            {
+                if(ReferenceEquals( estanteA.producto[i], null))
+                {
+                    bufferEstante = string.Concat(bufferEstante, Producto.MostrarProducto(estanteA.producto[i]));
+                }
+            }
+            return bufferEstante;
+        }
+
+        public static bool operator ==(Estante estante, Producto producto)
+        {
+            return (estante == producto);
+        }
+
+        public static bool operator !=(Estante estante, Producto producto)
+        {
+            return !(estante == producto);
+        }
+
+        public bool operator +(Estante estante, Producto producto)
+        {
+
+            for (int i=0; i<estante.producto.Length; i++)
+            {
+
+                
+            }
+        }
+
+        public Estante operator(Estante estante, Producto producto)
+        {
+
+
+            return estante.producto;
+        } 
+
+
+    }
+}
---
+++ Repaso/ConsoleApp2/Producto.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Producto
+    {
+
+        private string codigoDeBarras;
+        private string marca;
+        private float precio;
+
+        public string GetMarca()
+        {
+            return marca;
+        }
+
+        public float GetPrecio()
+        {
+            return precio;
+        }
+
+        public static Producto MostrarProducto(Producto producto)
+        {
+            
+
+            
+
+            return producto;
+        }
+
+        public static explicit operator string(Producto producto)
+        {
+            return producto.codigoDeBarras;
+        }
+
+        public static bool operator ==(Producto productoA, Producto productoB)
+        { 
+             return (productoA.marca == productoB.marca && productoA.codigoDeBarras == productoB.codigoDeBarras);
+        }
+
+        public static bool operator !=(Producto productoA, Producto productoB)
+        {
+            return !(productoA == productoB);
+        }
+
+        public static bool operator ==(Producto productoA, string productoString)
+        {
+            return (productoA.marca == productoString );
+        }
+
+        public static bool operator !=(Producto productoA, string productoSrting)
+        {
+            return !(productoA == productoSrting);
+        }
+
+
+
+    }
+}
