6515c54e34c408df2fb08f80fe7505a97ae9817a
tinchogodoy <martinjgodoy@gmail.com>
2018-09-26 09:23:47 -0300

    centralita reformada

--- Ejercicio 37/CentralitaHerencia/CentralitaHerencia/Centralita.cs
+++ Ejercicio 37/CentralitaHerencia/CentralitaHerencia/Centralita.cs
@@ -8,8 +8,18 @@ namespace CentralitaHerencia
 {
     public class Centralita
     {
-        private List<Llamada> ListaDeLlamadas;
+        private List<Llamada> listaDeLlamadas;
         protected string razonSocial;
+
+        public Centralita()
+        {
+            this.listaDeLlamadas = new List<Llamada>();
+        }
+
+        public Centralita(string nombreEmpresa)
+        {
+            this.razonSocial = nombreEmpresa;
+        }
 
         public float GananciasPorLocal
         {
@@ -21,19 +31,68 @@ namespace CentralitaHerencia
             }
 
         }
-        public float GananciasPorProvincia
+        public float GananciasPorProvincial
         {
             get
             {
-                float ganancia = Llamada.TipoLlamada();
+                float ganancia = 0;
                 return ganancia;
             }
         }
+        public float GananciasPorTotal
+        {
+            get
+            {
+                float ganancia = 0;
+                return ganancia;
+            }
+        }
+
+        public List<Llamada> Llamadas
+        {
+            get
+            {
+                List<Llamada> llamadas = new List<Llamada>();
+                listaDeLlamadas = llamadas;
+                             
+                return listaDeLlamadas;
+            }
+        }
+
         private float CalcularGanancia()
+        {
+            float ganancia = 0;
+            switch (Llamada.TipoLlamada.Local)
+            {
+
+                case Llamada.TipoLlamada.Local:
+
+                break;
+                case Llamada.TipoLlamada.Provincial:
+
+                break;
+                case Llamada.TipoLlamada.Todas:
+
+                break;
+                    
+            }
+
+            return ganancia;
+        }
+
+        public string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();// StringBuilder
+            sb.AppendFormat("Razon Social: {0} , Ganancia total: {1} , Ganancia llamadas locales {2}, Ganancia por llamadas provinciales {3}, ", this.razonSocial, this.GananciasPorTotal, this.GananciasPorLocal , this.GananciasPorProvincial);
+            sb.AppendFormat("detalles de llamadas: ");
+            
+            return sb.ToString();
+        }
+
+        public void OrdenarLlamadas()
         {
 
         }
-
          
     }
 }
